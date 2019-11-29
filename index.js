a60a6035708e28f7d8dc69790c7b7163f66706a1
orzoon <orzoonkunwar7.ak@gmail.com>
2019-11-29 19:38:35 +1100

    ..

--- index.js
+++ index.js
@@ -9,19 +9,24 @@ window.addEventListener('load', () => {
     }
     canvas.height = document.documentElement.clientHeight;
     canvas.width = document.documentElement.clientWidth;
+    let testArray = []
     let arrayListToSort = [];
     let totXLength = 0;
-    let lineSize = 5;
-    let lineXGap = 10;
-    let arrayLength = 50;
+    let lineSize = 1;
+    let lineXGap = 2;
+    let arrayLength = 300;
     let animating = true;
     let i = 0;
     let j = 0;
-    let selectionSortMIN;
+    let combSortGap = 0;
+    let combSortGapInitial = true;
+    let selectionSortMIN = 0;
+    let setInsertionLoop = true;
     for(s = 0; s < arrayLength; s++){
         arrayListToSort[s] = Math.floor(Math.random()* (canvas.height-100))
         totXLength = totXLength + lineXGap;
     }
+    testArray = [...arrayListToSort]
     function finalDraw(){
         c.clearRect(0,0,canvas.width, canvas.height)
         for(z = 0; z < arrayListToSort.length; z ++){
@@ -52,8 +57,14 @@ window.addEventListener('load', () => {
                 if(x === j){
                     c.strokeStyle = 'red'
                 }
+                else if(x === j-1){
+                    c.strokeStyle = 'blue'
+                }
+                else if(x > arrayListToSort.length-i-1){
+                    c.strokeStyle = 'green'
+                }
                 else {
-                    c.strokeStyle = '#000'
+                    c.strokeStyle = '#ddd'
                 }
                 c.stroke();
             }
@@ -73,12 +84,76 @@ window.addEventListener('load', () => {
             finalDraw()
         }
     }
+
+    function drawCombSort(){
+        if(animating){
+            requestAnimationFrame(drawCombSort);
+            c.clearRect(0,0,canvas.width, canvas.height)
+            if(combSortGapInitial){
+                combSortGap = arrayListToSort.length;
+                combSortGapInitial = false;
+            }
+          
+            if(combSortGap < 1){
+                combSortGap = 1;
+            }
+            //draw
+            for(x = 0; x < arrayListToSort.length; x ++){
+                c.beginPath();
+                c.moveTo(canvas.width/2 - totXLength/2 + (x * lineXGap), 0);
+                c.lineTo(canvas.width/2 - totXLength/2 + (x * lineXGap),arrayListToSort[x]);
+                c.lineWidth = lineSize;
+                if(x === j){
+                    c.strokeStyle = 'red'
+                }
+                else if(x === j+combSortGap){
+                    c.strokeStyle = 'blue'
+                }
+                else {
+                    c.strokeStyle = '#ddd'
+                }
+                c.stroke();
+            }
+            if(j + combSortGap > arrayListToSort.length){
+                i++;
+                j = 0;
+                combSortGap = Math.floor(combSortGap/1.25);
+            } 
+            if(arrayListToSort[j] > arrayListToSort[j+combSortGap]){
+                let temp = arrayListToSort[j];
+                arrayListToSort[j] = arrayListToSort[j + combSortGap]
+                arrayListToSort[j+combSortGap] = temp;
+            }
+            j++;
+            if(i > arrayListToSort.length-2 || combSortGap === 0){
+                animating = false;
+                return;
+            }
+        }
+        else {
+            console.log("finished")
+            finalDraw();
+        }
+    }
     function drawSelectionSort(){
         if(animating){
             requestAnimationFrame(drawSelectionSort);
             // clearing canvas
             c.clearRect(0,0,canvas.width, canvas.height);
             // drawing list on canvas
+            // checking the list
+            if(j >= arrayListToSort.length){
+                let temp = arrayListToSort[i];
+                arrayListToSort[i] = arrayListToSort[selectionSortMIN]
+                arrayListToSort[selectionSortMIN] = temp;
+                i++;
+                j = i;
+                selectionSortMIN = i;
+            }
+
+            if(arrayListToSort[j+1] < arrayListToSort[selectionSortMIN]){
+                selectionSortMIN = j+1;
+            }
             for(let x = 0; x < arrayListToSort.length; x ++){
                 c.beginPath();
                 c.moveTo(canvas.width/2 - totXLength/2 + (x * lineXGap), 0);
@@ -99,31 +174,73 @@ window.addEventListener('load', () => {
                // c.strokeStyle = 'red';
                 c.stroke();
             }
-            // checking the list
-            if(j > arrayListToSort.length){
-                let temp = arrayListToSort[i];
-                arrayListToSort[i] = arrayListToSort[selectionSortMIN]
-                arrayListToSort[selectionSortMIN] = temp;
-                i++;
-                j = i;
-                selectionSortMIN = i;
-            }
-
-            if(arrayListToSort[j+1] < arrayListToSort[selectionSortMIN]){
-                selectionSortMIN = j+1;
-            }
             j++;
+            if(i >= arrayListToSort.length){
+                animating = false
+            }
         }
         else{
-            console.log("finisshed")
+            console.log("finished")
             finalDraw();
         }
     }
-    requestAnimationFrame(drawSelectionSort)
-
+    function drawInsertionSort(){
+        if(animating){
+            requestAnimationFrame(drawInsertionSort);
+            c.clearRect(0,0,canvas.width, canvas.height);
+            if(i > arrayListToSort.length-1){
+                animating = false;
+                return false
+            }
+            //draw;
+            for(let x = 0; x < arrayListToSort.length; x ++){
+                c.beginPath();
+                c.moveTo(canvas.width/2 - totXLength/2 + (x * lineXGap), 0);
+                c.lineTo(canvas.width/2 - totXLength/2 + (x * lineXGap),arrayListToSort[x]);
+                c.lineWidth = lineSize;
+                if(x === i){
+                    c.strokeStyle = 'red'
+                }
+                else if(x === j){
+                    c.strokeStyle = "blue"
+                }
+                else if (x < i) {
+                    c.strokeStyle = "#90ee90"
+                }
+                else {
+                    c.strokeStyle = "#ddd"
+                }
+                c.stroke();
+            }
+            if(setInsertionLoop === true){
+                i++;
+                j = i;
+                setInsertionLoop = false;
+            }
+            if(j > 0 && arrayListToSort[j-1] > arrayListToSort[j]){
+                let temp = arrayListToSort[j-1];
+                arrayListToSort[j-1] = arrayListToSort[j]
+                arrayListToSort[j] = temp;
+                j--
+            }
+            else {
+                setInsertionLoop = true
+            }
+            
+          
+        }
+        else {
+            console.log("finished");
+            finalDraw();
+        }
+    }
     //requestAnimationFrame(drawBubbleSort)
+    //requestAnimationFrame(drawCombSort)
+    //requestAnimationFrame(drawSelectionSort)
+    //requestAnimationFrame(drawInsertionSort)
     /*------------------------------------*/
     /*---------SORTING FUNCTIONS-----------*/
+     /*--------------BUBBLE SORT----------*/
     function bubbleSort(arrayListToSort){
         for(let i = 0; i < arrayListToSort.length; i++){
                 for(let j = 0; j < arrayListToSort.length - i; j++ ){
@@ -135,11 +252,26 @@ window.addEventListener('load', () => {
                 }
         }
     }
+    /*--------------COMB SORT----------*/
+    function combSort(array){
+        let sortGap = array.length;
+        for(let a = 0 ; a < array.length - 1; a++){
+            for(let b = 0; b+sortGap < array.length; b++){
+                if(array[b] > array[b+ sortGap]){
+                    let temp = array[b];
+                    array[b] = array[b+sortGap];
+                    array[b+sortGap] = temp;
+                }
+            }
+            sortGap = Math.floor(sortGap/1.3);
+        }
+        console.log(array)
+    }
 
     /*--------------SELECTION SORT----------*/
     function selectionSort(array){
         let MIN;
-        for(a = 0; a < array.length ; a++){
+        for(let a = 0; a < array.length ; a++){
             MIN = a;
             for(let b = a; b < array.length; b++){
                 if(array[b+1] < array[MIN]){
@@ -151,8 +283,59 @@ window.addEventListener('load', () => {
             array[MIN] = temp;
         }
     }
+
+    /*--------------INSERTION SORT------------*/
+    function insertionSort(array){
+        let MIN;
+        for(a = 0; a < array.length-1; a++){
+           MIN = a;
+           for(let b = a + 1; b > 0; b--){
+               if(array[b] >= array[MIN]){
+                   break;
+               }
+               if(array [b] < array[MIN]){
+                   let temp = array[MIN]
+                   array[MIN] = array[b]
+                   array[b] = temp;
+                   MIN = MIN - 1
+               }
+           }
+        }
+    }
+    function mergeSort(array){
+        if(array.length === 1 ){
+            return array
+        }
+        let middle = Math.floor(array.length/2)
+        let left = array.slice(0,middle);
+        let right = array.slice(middle);
+        return merge(mergeSort(left), mergeSort(right))
+    }
+    function merge(left,right){
+        let mergedArray = [];
+        let leftIndex = 0;
+        let rightIndex = 0;
+        while(leftIndex < left.length && rightIndex < right.length){
+            if(left[leftIndex] < (right[rightIndex])){
+                mergedArray.push(left[leftIndex])
+                leftIndex++;
+            }
+            else{
+                mergedArray.push(right[rightIndex])
+                rightIndex++;
+            }
+        }
+  
+        let newArray = mergedArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+        console.log(newArray)
+        return newArray
+
+    }
+   
+   
     /*--------SORTING CALLS-----------------*/
     /*-------CALLING BUBBLE SORT-----------*/
-    //drawBubbleSort();
-    
+ 
+
+ 
 })
